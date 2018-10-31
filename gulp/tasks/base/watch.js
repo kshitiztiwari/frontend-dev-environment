4964b14167324999ce3386e37943bc09689d297a
Kshitiz Tiwari <tiwarikshitiz@gmail.com>
2018-10-31 13:08:58 +0545

    Tasks and gulpfile update

--- gulp/tasks/base/watch.js
+++ gulp/tasks/base/watch.js
@@ -2,10 +2,31 @@
 
 module.exports = function(gulp, $, path, config) {
 
+	var changeListener = function(path){
+		// console.log( path + ' changed.')
+        $.util.log( $.util.colors.yellow( path ) + ' changed.' );
+	}
+
     gulp.task('watch', function() {
-        gulp.watch( path.sass.src, ['sass']);
-        gulp.watch( path.nunjucks.watch.pages, ['nunjucks']);
-        gulp.watch( path.nunjucks.watch.templates, ['nunjucks:template']);
-    });
-    
+        
+        var sass = $.watch( path.sass.src, config.watch, function(){
+        	gulp.start('sass');
+        });
+
+        var template = $.watch( path.template.watch.pages,  config.watch, function(){
+        	gulp.start('template');
+        });
+
+        var template_template = $.watch( path.template.watch.templates,  config.watch, function(){
+            gulp.start('template:template');
+        });       
+
+        var js = $.watch( path.js.watch,  config.watch, function(){
+        	gulp.start('webpack:js');
+        });      
+        
+        var assets = $.watch( path.assets.watch, config.watch, function() {
+            gulp.start( 'assets' );
+        } );
+    });  
 };
